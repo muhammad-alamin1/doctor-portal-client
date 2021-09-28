45ecf1feed3b805e96abd1341882809007df66a6
Muhammad Al-amin <muhammad.alamindev01@gmail.com>
2021-09-28 19:47:04 +0600

    doctor portal

--- src/Components/Home/Doctor/Doctor.js
+++ src/Components/Home/Doctor/Doctor.js
@@ -1,14 +1,18 @@
 import React, { useContext } from 'react';
-import doctor from '../../../images/appointment-doctor.png';
+import img from '../../../images/appointment-doctor.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
 
 
-const Doctor = () => {
+const Doctor = ({doctor}) => {
     
     return (
         <div className="doctor text-center col-md-4" style={{marginBottom:'100px'}}>
-            <img src={doctor} alt="doctor" className="img-fluid" />
+            
+                {/* doctor.image ? <img src={`data:image/png;base64,${doctor.image.img}`} alt=""/> 
+                :  */}
+                <img src={img} alt="doctor" className="img-fluid" />
+            
             <h5 className="my-3">Dr. Coudi</h5>
             <p> <FontAwesomeIcon className="text-primary" icon={faPhoneAlt}/> +880-188-934789</p>
         </div>
